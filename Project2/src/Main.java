2640c446932181014699dda5d2b6553602c5028f
LouaiZahran <louai0nasr@gmail.com>
2022-04-25 06:23:38 +0200

    Made InputManager static
    Its functions now RETURN the structure requested

--- Project2/src/Main.java
+++ Project2/src/Main.java
@@ -1,7 +1,16 @@
 import components.InputGenerator;
+import components.InputManager;
+import components.Obj;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args){
-        InputGenerator.generate();
+    public static void main(String[] args) throws IOException {
+        HashMap<Integer, Obj> map = InputManager.getObjects("heap.csv");
+        for(Map.Entry<Integer, Obj> entry: map.entrySet())
+            System.out.println(entry.getKey());
     }
 }
