6eee01bca61d4c275dcf260981a2affc97006c72
LouaiZahran <louai0nasr@gmail.com>
2022-04-25 06:43:56 +0200

    Update Heap.java

--- Project2/src/components/Heap.java
+++ Project2/src/components/Heap.java
@@ -4,24 +4,23 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Heap {
-    // id --> object
-    public HashMap<Integer, Obj> objects;
-    // id --> ids
-    public HashMap<Integer,ArrayList<Integer>> network;
-    // needed objects 
-    public ArrayList<Integer> activeIds;
+    public HashMap<Integer, Obj> objects;                   // id --> object
+    public HashMap<Integer,ArrayList<Integer>> network;     // id --> ids
+    public ArrayList<Integer> activeIds;                    // needed objects
+
     public Heap(){
         this.objects=new HashMap<>();
         this.network=new HashMap<>();
         this.activeIds = new ArrayList<>();
     }
-    public Heap(InputManager i, String objects_f, String network_f, String activeids_f){
+    
+    public Heap(String objects_f, String network_f, String activeids_f){
         this.objects=new HashMap<>();
         this.network=new HashMap<>();
         try {
-            this.activeIds = i.getActiveObjects(activeids_f);
-            this.objects = i.getObjects(objects_f);
-            this.network = i.getNetwork(network_f);
+            this.activeIds = InputManager.getActiveObjects(activeids_f);
+            this.objects = InputManager.getObjects(objects_f);
+            this.network = InputManager.getNetwork(network_f);
         } catch (IOException e) {
             e.printStackTrace();
         }
