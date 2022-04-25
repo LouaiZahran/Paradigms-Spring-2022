12e1d06e8678689f6b5301f4f2514da6fc829d2d
3amo-magdy <amoramgadymahmoud@gmail.com>
2022-04-26 00:43:08 +0200

    mark_sweep (O(n instead of n^2))

--- Project2/src/GarbageCollectors/mark_sweep.java
+++ Project2/src/GarbageCollectors/mark_sweep.java
@@ -21,16 +21,15 @@ public class mark_sweep{
     public static void collect(Heap heap){
         HashMap<Integer,Boolean> marked = new HashMap<>();
         //marking
-        for (Integer id : heap.objects.keySet()) {
-            if(heap.activeIds.contains(id)){
-                mark(id, heap, marked);
-            }
-            else{
-                Boolean a = marked.get(id);
-                Boolean b = (a == null)? false : a;
-                marked.put(id, b);
-            }
+        for(Integer id : heap.activeIds){
+            mark(id, heap, marked);
         }
+        for (Integer id : heap.objects.keySet()) {
+            Boolean a = marked.get(id);
+            Boolean b = (a == null)? false : a;
+            marked.put(id, b);
+        }
+        //sweeping
         for(Entry<Integer,Boolean> e :marked.entrySet()){
             if(!marked.get(e.getKey())){
                 heap.objects.remove(e.getKey());
