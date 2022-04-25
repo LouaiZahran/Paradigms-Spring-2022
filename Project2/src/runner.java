bb34b479ec72faefa1bde348f374eb6a4071b33e
3amo-magdy <amoramgadymahmoud@gmail.com>
2022-04-25 05:48:12 +0200

    .

--- Project2/src/runner.java
+++ Project2/src/runner.java
@@ -2,7 +2,7 @@ import components.InputManager;
 import components.heap;
 
 public class runner {
-    public static void main(){
+    public static void main(String[]a){
         InputManager i = new InputManager();
         heap Heap = new heap(i,"","","");
 
