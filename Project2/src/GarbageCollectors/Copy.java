602ef80f81e90848100ab843ee7f30b4d98586c1
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2022-04-28 17:15:52 +0200

    copy problem fixed
    visited before added

--- Project2/src/GarbageCollectors/Copy.java
+++ Project2/src/GarbageCollectors/Copy.java
@@ -5,12 +5,12 @@ import components.Obj;
 import components.OutputManager;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Copy {
     private static Heap resultHeap;
     private static int addRoot(Heap oldHeap,Heap newHeap,Integer root,int endPtr){
+        if(newHeap.objects.containsKey(root))
+            return endPtr;
         int start=oldHeap.objects.get(root).getStart();
         int end=oldHeap.objects.get(root).getEnd();
         int size=end-start;
@@ -22,9 +22,9 @@ public class Copy {
         if(oldHeap.network.get(root)==null)
             return endPtr;
 
-       for(Integer child : oldHeap.network.get(root)) {
-           if(newHeap.objects.containsKey(child))
-               continue;
+        for(Integer child : oldHeap.network.get(root)) {
+            if(newHeap.objects.containsKey(child))
+                continue;
             int start=oldHeap.objects.get(child).getStart();
             int end=oldHeap.objects.get(child).getEnd();
             int size=end-start;
@@ -38,7 +38,6 @@ public class Copy {
     public static void collect(Heap oldHeap){
         Heap newHeap=new Heap();
         int endPtr=0;
-
         for(Integer active : oldHeap.activeIds){
             endPtr=addRoot(oldHeap,newHeap,active,endPtr);
             endPtr=addChildren(oldHeap,newHeap,active,endPtr);
