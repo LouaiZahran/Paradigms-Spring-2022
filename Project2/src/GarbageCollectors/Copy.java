19a6c4e2b0ada8f529d824ba3513292f2767af05
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2022-04-28 20:26:16 +0200

    Copy changed to BFS with its tests

--- Project2/src/GarbageCollectors/Copy.java
+++ Project2/src/GarbageCollectors/Copy.java
@@ -5,42 +5,35 @@ import components.Obj;
 import components.OutputManager;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Copy {
     private static Heap resultHeap;
-    private static int addRoot(Heap oldHeap,Heap newHeap,Integer root,int endPtr){
-        if(newHeap.objects.containsKey(root))
-            return endPtr;
-        int start=oldHeap.objects.get(root).getStart();
-        int end=oldHeap.objects.get(root).getEnd();
-        int size=end-start;
-        newHeap.objects.put(root,new Obj(root,endPtr,endPtr+size));
-        endPtr=endPtr+size+1;
-        return endPtr;
-    }
-    private static int addChildren(Heap oldHeap,Heap newHeap,Integer root,int endPtr){
-        if(oldHeap.network.get(root)==null)
-            return endPtr;
-
-        for(Integer child : oldHeap.network.get(root)) {
-            if(newHeap.objects.containsKey(child))
-                continue;
-            int start=oldHeap.objects.get(child).getStart();
-            int end=oldHeap.objects.get(child).getEnd();
-            int size=end-start;
-            newHeap.objects.put(child,new Obj(child,endPtr,endPtr+size));
-            endPtr=endPtr+size+1;
-            endPtr=addChildren(oldHeap,newHeap,child,endPtr);
-        }
-        return endPtr;
-    }
 
     public static void collect(Heap oldHeap){
         Heap newHeap=new Heap();
+        Queue<Integer> queue=new LinkedList<>();
         int endPtr=0;
-        for(Integer active : oldHeap.activeIds){
-            endPtr=addRoot(oldHeap,newHeap,active,endPtr);
-            endPtr=addChildren(oldHeap,newHeap,active,endPtr);
+        for(Integer active :oldHeap.activeIds)
+            queue.add(active);
+
+        while (queue.size()!=0){
+            Integer curr=queue.poll();
+            if(oldHeap.network.get(curr)!=null) {
+                for (Integer child : oldHeap.network.get(curr)) {
+                    if (newHeap.objects.containsKey(curr)) //skip child already added
+                        continue;
+                    queue.add(child);
+                }
+            }
+            if(newHeap.objects.containsKey(curr)) // skip root already added
+                continue;
+            int start=oldHeap.objects.get(curr).getStart();
+            int end=oldHeap.objects.get(curr).getEnd();
+            int size=end-start;
+            newHeap.objects.put(curr,new Obj(curr,endPtr,endPtr+size));
+            endPtr=endPtr+size+1;
         }
         resultHeap=newHeap;
     }
